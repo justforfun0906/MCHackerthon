0a7c553f3cfa777dd4cf4c92938eea3b3fe49fa0
johiny530 <johiny530@gapp.nthu.edu.tw>
2025-09-18 15:20:01 +0000

    加額外資訊

--- components/JobSection.js
+++ components/JobSection.js
@@ -10,17 +10,31 @@ const JobSection = {
                     class="job-item"
                     @click="selectJob(job)"
                 >
-                    <div class="job-info">
-                        <span>{{ job.region }}｜{{ job.storeType }}</span>
-                        <span class="job-reward">{{ job.count }}人</span>
-                    </div>
-                    <div class="job-info">
-                        <span>需求：{{ job.roles.join('、') }}</span>
-                        <span>{{ job.time }}</span>
-                    </div>
-                    <div v-if="job.note" class="job-note">
-                        <span>備註：{{ job.note }}</span>
-                    </div>
+                    <template v-if="$parent && $parent.currentTab === 'mine' && $parent.role === 'employer'">
+                        <div class="job-info"><span>地區：{{ job.region }}</span></div>
+                        <div class="job-info"><span>店鋪類型：{{ job.storeType }}</span></div>
+                        <div class="job-info"><span>地址：{{ job.address || '（未填）' }}</span></div>
+                        <div class="job-info"><span>人數：{{ job.count }}</span></div>
+                        <div class="job-info"><span>需求：{{ job.roles.join('、') }}</span></div>
+                        <div class="job-info"><span>時段：{{ job.time }}</span></div>
+                        <div class="job-info" v-if="job.note"><span>備註：{{ job.note }}</span></div>
+                    </template>
+                    <template v-else>
+                        <div class="job-info">
+                            <span>{{ job.region }}｜{{ job.storeType }}</span>
+                            <span class="job-reward">{{ job.count }}人</span>
+                        </div>
+                        <div v-if="job.address" class="job-info">
+                            <span>地址：{{ job.address }}</span>
+                        </div>
+                        <div class="job-info">
+                            <span>需求：{{ job.roles.join('、') }}</span>
+                            <span>{{ job.time }}</span>
+                        </div>
+                        <div v-if="job.note" class="job-note">
+                            <span>備註：{{ job.note }}</span>
+                        </div>
+                    </template>
                 </div>
                 <div v-if="jobs.length === 0" class="job-item">目前沒有符合條件的職缺</div>
             </div>
